120fc439b6c8998288b9cd891f12953753fcfda3
unknown <leahlt@gmail.com>
2021-06-15 09:32:49 -0700

    added space failsafe for compression

--- compression.v
+++ compression.v
@@ -9,6 +9,8 @@ assign OUT = ((IN - 6'b100_000) == 8'd130) ? 7'd95 : //cent
 						((IN - 6'b100_000) == 8'd137) ? 7'd111 : //copyright
 						((IN - 6'b100_000) == 8'd142) ? 7'd112 : //r in circle
 						((IN - 6'b100_000) == 8'd144) ? 7'd125 : //degree
+						(IN < 8'd32) ? 7'd0:
+						(IN > 8'd159) ? 7'd0:
 						(IN - 6'b100_000); //do nothing
 
 						
