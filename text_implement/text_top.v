9fb6e38b8758624fcbe2d9d5131897eb8c337f34
danielmutong <48365524+danielmutong@users.noreply.github.com>
2021-06-15 12:29:00 -0700

    Text donewith new bpsk

--- text_implement/text_top.v
+++ text_implement/text_top.v
@@ -1,11 +1,11 @@
-module text_top(input clk, input reset,/*input [7:0] data_in,*/ output [7:0] data_out);
+module text_top(input clk, input reset, output [7:0] data_out);
     wire [7:0] source_text;
-    wire [11:0] channel_data_out;
+    wire [23:0] channel_data_out;
     wire [6:0] encrypted_txt;
     wire [6:0] decrypted_txt;
     wire [7:0] data_in;
     wire err_corrected, err_detected, err_fatal;
-    wire [11:0] mod_out;
+    wire [23:0] mod_out;
     wire [11:0] demod_out;
     wire [11:0] compressed_out;
     wire [11:0] hamming_enc;
@@ -15,11 +15,11 @@ module text_top(input clk, input reset,/*input [7:0] data_in,*/ output [7:0] dat
     encrypt encryptor(clk, rst, 123, compressed_out, encrypted_txt, init_done);
 
     HammingIP_Enc hamming_encoder(encrypted_txt, hamming_enc);
-    BPSK modulator(hamming_enc, mod_out,clk,  1);
+    BPSK_mod modulator(hamming_enc, mod_out);
 
     channel channel_mod(clk, reset, mod_out, channel_data_out);
 
-    BPSK demodulator(channel_data_out, demod_out, clk, 1);
+    BPSK_demod demodulator(channel_data_out, demod_out);
     HammingIP_Dec hamming_decoder(channel_data_out,err_corrected,err_detected,err_fatal,hamming_dec);
     decrypt decryptor(clk, rst, 123, demod_out, decrypted_txt, init_done);
     decompression decompressd(decrypted_txt, data_out);
